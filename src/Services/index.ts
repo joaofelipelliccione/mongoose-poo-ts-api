58b86406a8b21967c119fa5e7bbbe7a207eb1362
João Felipe P <jf.pelliccione@gmail.com>
2022-04-18 20:22:24 -0300

    Começando a estruturação dos Services.

---
+++ src/Services/index.ts
@@ -0,0 +1,25 @@
+import { ZodError } from 'zod';
+import Model from '../Models';
+
+// Criação de uma interface de erro, tendo em vista que o retorno de alguns métodos de Service, poderão ser erros lançados pelo Zod.
+export interface ServiceError {
+  error: ZodError;
+}
+
+abstract class Service<T> {
+  constructor(protected model: Model<T>) { }
+
+  public async create(obj: T): Promise<T | null | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  public async read(): Promise<T[]> {
+    return this.model.read();
+  }
+
+  public async readOne(id: string): Promise<T | null | ServiceError> {
+    return this.model.readOne(id);
+  }
+}
+
+export default Service;
